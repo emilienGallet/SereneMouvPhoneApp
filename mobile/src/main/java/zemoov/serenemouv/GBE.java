68a555cfe01bf69548dd08a175416adee85320c2
mell421 <43302304+mell421@users.noreply.github.com>
2021-02-27 21:48:13 +0100

    add parser csv

--- mobile/src/main/java/zemoov/serenemouv/GBE.java
+++ mobile/src/main/java/zemoov/serenemouv/GBE.java
@@ -1,31 +1,49 @@
 package zemoov.serenemouv;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+// import java.io.File;
+// import java.io.FileNotFoundException;
+import java.util.Scanner;
+ 
+public class GBE {
+    String n_amenageur,n_operateur,n_enseigne,id_station,n_station,ad_station;
+    int code_insee;
+    double Xlongitude,Ylatitude;
+    int nbre_pdc;
+    String id_pdc;
+    int puiss_max;
+    String type_prise,acces_recharge,accessibilité,observations;
+    Date date_maj;
+    String source;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+    public GBE(){}
 
-//import com.google.android.gms.maps.*;
-//import com.google.android.libraries.places.api.*;
+    public GBE(String n_amenageur,String n_operateur,String n_enseigne,String id_station,String n_station,
+        String ad_station,int code_insee,double Xlongitude,double Ylatitude,int nbre_pdc,String id_pdc,
+        int puiss_max,String type_prise,String acces_recharge,String accessibilité,String observations,Date date_maj,String source){
+            this.n_amenageur=n_amenageur;//0
+            this.n_operateur=n_operateur;//1
+            this.n_enseigne=n_enseigne; //2
+            this.id_station=id_station;//3
+            this.n_station=n_station;//4
+            this.ad_station=ad_station;//5
+            this.code_insee=code_insee;//6
+            this.Xlongitude=Xlongitude;//7
+            this.Ylatitude=Ylatitude;//8
+            this.nbre_pdc=nbre_pdc;//9
+            this.id_pdc=id_pdc;//10
+            this.puiss_max=puiss_max;//11
+            this.type_prise=type_prise;//12
+            this.acces_recharge=acces_recharge;//13
+            this.accessibilité=accessibilité;//14
+            this.observations=observations;//15
+            this.date_maj=date_maj;//16
+            this.source=source;//17
+    }
 
-public abstract class GBE extends AppCompatActivity implements OnMapReadyCallback { //implements OnMapReadyCallback
-    private GoogleMap mMap;
-    private View mapView;
-    private Button btnFind;
-    // private Object OnMapReadyCallback;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-
-        btnFind = findViewById(R.id.find);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync((OnMapReadyCallback) this);
-        mapView = mapFragment.getView();
+    public static int main(String[] args) {
+        csvFile csvFile = new csvFile();
+        GBE gbe = new GBE();
+        csvFile.readfile();
+        return 1;
     }
 }
---
+++ mobile/src/main/java/zemoov/serenemouv/ListBorns.java
@@ -0,0 +1,3 @@
+package zemoov.serenemouv;
+
+
