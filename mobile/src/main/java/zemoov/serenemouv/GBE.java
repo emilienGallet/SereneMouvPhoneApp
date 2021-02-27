5594af385a4972e1f9af5803411ccb83aca47887
mell421 <43302304+mell421@users.noreply.github.com>
2021-02-27 14:05:22 +0100

    update bug

--- mobile/src/main/java/zemoov/serenemouv/GBE.java
+++ mobile/src/main/java/zemoov/serenemouv/GBE.java
@@ -5,22 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+//import com.google.android.gms.maps.*;
 //import com.google.android.libraries.places.api.*;
 
-public class GBE extends AppCompatActivity  { //implements OnMapReadyCallback
+public abstract class GBE extends AppCompatActivity implements OnMapReadyCallback { //implements OnMapReadyCallback
     private GoogleMap mMap;
     private View mapView;
     private Button btnFind;
+    // private Object OnMapReadyCallback;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        //btnFind = findViewById(R.id.btn_find);
-
-        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        //mapFragment.getMapAsync(this);
-        //mapView = mapFragment.getView();
+        btnFind = findViewById(R.id.find);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+        mapView = mapFragment.getView();
     }
 }
