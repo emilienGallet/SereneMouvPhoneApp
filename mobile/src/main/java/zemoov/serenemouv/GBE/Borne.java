f4f1b2c18826e95c5d627fc1f289cff05b7e6cb7
mell421 <43302304+mell421@users.noreply.github.com>
2021-03-13 13:07:12 +0100

    add getter, continue BornesAutour

--- mobile/src/main/java/zemoov/serenemouv/GBE/Borne.java
+++ mobile/src/main/java/zemoov/serenemouv/GBE/Borne.java
@@ -3,7 +3,7 @@ package zemoov.serenemouv.GBE;
 import zemoov.serenemouv.CMTA.Localisation;
 import zemoov.serenemouv.CMTA.Vehicule;
 
-public class Borne {
+public class Borne extends GBE {
 
     public Localisation position;
 
--- mobile/src/main/java/zemoov/serenemouv/GBE/GBE.java
+++ mobile/src/main/java/zemoov/serenemouv/GBE/GBE.java
@@ -6,19 +6,19 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.graalvm.compiler.nodes.extended.GetClassNode;
+
 import zemoov.serenemouv.CMTA.Cmta;
 import zemoov.serenemouv.CMTA.Trajet;
 
 public class GBE {
-    String n_amenageur,n_operateur,n_enseigne,id_station,n_station,ad_station;
-    int code_insee;
-    double Xlongitude,Ylatitude;
-    int nbre_pdc;
-    String id_pdc;
-    int puiss_max;
-    String type_prise,acces_recharge,accessibilite,observations;
-    String date_maj;
-    String source;
+    public String n_amenageur,n_operateur,n_enseigne,id_station,n_station,ad_station;
+    public int code_insee;
+    public double Xlongitude,Ylatitude;
+    public int nbre_pdc;
+    public String id_pdc;
+    public int puiss_max;
+    public String type_prise,acces_recharge,accessibilite,observations;
 
     // @Override
     // protected void onCreate(Bundle savedInstanceState) {
@@ -49,42 +49,102 @@ public class GBE {
             this.acces_recharge=acces_recharge;//13
             this.accessibilite=accessibilite;//14
             this.observations=observations;//15
-            this.date_maj=date_maj;//16
-            this.source=source;//17
     }
 
-    public static int main(String[] args) {
+    public static void main(String[] args) {
         CsvFile csvFile = new CsvFile();
         GBE gbe = new GBE();
         csvFile.readfile();
-        return 1;
     }
+   
+    // getter,setter
+    public String getAcces_recharge() {
+        return acces_recharge;
+    }
+    public String getAccessibilite() {
+        return accessibilite;
+    }
+    public String getAd_station() {
+        return ad_station;
+    }
+    public int getCode_insee() {
+        return code_insee;
+    }
+    public String getId_pdc() {
+        return id_pdc;
+    }
+    public String getId_station() {
+        return id_station;
+    }
+    public String getN_amenageur() {
+        return n_amenageur;
+    }
+    public String getN_enseigne() {
+        return n_enseigne;
+    }
+    public String getN_operateur() {
+        return n_operateur;
+    }
+    public String getN_station() {
+        return n_station;
+    }
+    public int getNbre_pdc() {
+        return nbre_pdc;
+    }
+    public String getObservations() {
+        return observations;
+    }
+    public int getPuiss_max() {
+        return puiss_max;
+    }
+    public String getType_prise() {
+        return type_prise;
+    }
+    public double getXlongitude() {
+        return Xlongitude;
+    }
+    public double getYlatitude() {
+        return Ylatitude;
+    }
+    
 
+    // fct
     /**
      * Liste de borne UTILISABLE, ORDONÉE du départ jusqu'à l'arrivée
      * @param lesConfigUser correspondant au configuration utilisateur
      *                      (véhicule,badgesPossible,puissanceMax,puissanceMin)
      * @return
      */
-    public static ArrayList<Borne> bornesAutourDuTrajet(Cmta lesConfigUser,Borne borne) {
+    public static ArrayList<Borne> bornesAutourDuTrajet(Cmta lesConfigUser) {
         Borne borne2;
         double distance, limiteDist=10;
+        Cmta chemin;
+        int count=0;
         ArrayList<Borne> bornesP = new ArrayList<>();
         // on récupère les points du trajet
         for(int i = 0 ;i< lesConfigUser.unChemin.getPoints().length;i++){
             Position pos = lesConfigUser.unChemin.getPoints().get(0);
-            // calcul des bornes présentes dans les 10km alentours
-            distance = distance(lat1,lon1,lat2,lon2) ;
-            // borne.position.latitude && borne.position.longitude
-            if(distance < limiteDist){
-                // ajoute la ligne ds un tableau des bornes proches
-
+            Position posData;
+            // pour chaque ligne du csv lue
+            for(int j =0;j<100;j++){
+                // borne ligne j
+                posData = borne2.position.getPoints();
+                // calcul des bornes présentes dans les 10km alentours
+                distance = distance(pos.latitude,pos.longitude,posData.latitude,posData.longitude) ;
+                // borne.position.latitude && borne.position.longitude
+                if(distance < limiteDist){
+                    // ajoute la ligne ds un tableau des bornes proches
+                    bornesP.add(borne2);
+                    count++;
+                }
             }
+            
             
             
         }
         // affiche les bornes proches
-        return null;
+
+        return bornesP;
     }
 
     /**
@@ -105,10 +165,7 @@ public class GBE {
                 + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
 			dist = Math.acos(dist);
 			dist = Math.toDegrees(dist);
-			dist = dist * 60 * 1.1515;
-			// en KM
-			dist = dist * 1.609344;
-			
+			dist = dist * 60 * 1.1515 * 1.609344; // en KM
 			return dist;
 		}
 	}
